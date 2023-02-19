19587e44ffac532ba12e330ac500806ac55a4cda
CodedRed <devcodedred@gmail.com>
2023-02-18 21:31:05 -0600

    Quick data fix

--- src/main/java/me/codedred/playtimes/data/DataManager.java
+++ src/main/java/me/codedred/playtimes/data/DataManager.java
@@ -2,12 +2,13 @@ package me.codedred.playtimes.data;
 
 import me.codedred.playtimes.PlayTimes;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class DataManager {
 
 	private static final DataManager instance = new DataManager();
-    private final CustomConfig data = new CustomConfig(PlayTimes.getPlugin(PlayTimes.class), "data.yml");
-    private final CustomConfig cfg = new CustomConfig(PlayTimes.getPlugin(PlayTimes.class), "config.yml");
+    private final CustomConfig data = new CustomConfig(JavaPlugin.getPlugin(PlayTimes.class), "data.yml");
+    private final CustomConfig cfg = new CustomConfig(JavaPlugin.getPlugin(PlayTimes.class), "config.yml");
 
     public static DataManager getInstance() {
         return instance;
