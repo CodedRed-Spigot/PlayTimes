899f04967fb886d24ecef5686690e78d0cfdc726
CodedRed <devcodedred@gmail.com>
2023-02-21 13:31:22 -0600

    Added timezone to TimeFormatter

--- src/main/java/me/codedred/playtimes/utils/TimeFormatterUtil.java
+++ src/main/java/me/codedred/playtimes/utils/TimeFormatterUtil.java
@@ -5,17 +5,21 @@ import me.codedred.playtimes.data.DataManager;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class TimeFormatterUtil {
 
+    private static final DataManager dataManager = DataManager.getInstance();
+    private static final String FORMAT = dataManager.getConfig().getString("playtime.format");
+    private static final Locale LOCALE = Locale.forLanguageTag(dataManager.getConfig().getString("playtime.locale"));
+    private static final TimeZone TIMEZONE = TimeZone.getTimeZone(dataManager.getConfig().getString("playtime.timezone"));
+    private static final long MILLISECONDS_PER_SECOND = 1000;
+
+    private static final SimpleDateFormat SDF = new SimpleDateFormat(FORMAT, LOCALE);
 
     private TimeFormatterUtil() {
         throw new IllegalStateException("Utility class");
     }
-
-    private static final String FORMAT = DataManager.getInstance().getConfig().getString("playtime.format");
-    private static final Locale LOCALE = Locale.forLanguageTag(DataManager.getInstance().getConfig().getString("playtime.locale"));
-
 
     /**
      * Converts the given time in seconds to a formatted date string based on the provided date format, locale,
@@ -24,9 +28,9 @@ public class TimeFormatterUtil {
      * @param timeInSeconds the time in seconds to be converted
      * @return the formatted date string
      */
-    public static String secondsToFormattedTime(long timeInSeconds) {
-        SimpleDateFormat sdf = new SimpleDateFormat(FORMAT, LOCALE);
-        Date result = new Date(timeInSeconds * 1000);
-        return sdf.format(result);
+    public static synchronized String secondsToFormattedTime(long timeInSeconds) {
+        SDF.setTimeZone(TIMEZONE);
+        Date result = new Date(timeInSeconds * MILLISECONDS_PER_SECOND);
+        return SDF.format(result);
     }
 }
