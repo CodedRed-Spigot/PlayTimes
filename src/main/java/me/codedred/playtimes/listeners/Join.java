4d92c2c9addf4e336565c2ad3dcb7efc8617fe30
CodedRed <devcodedred@gmail.com>
2023-07-10 16:41:50 -0500

    fixed join bug

--- src/main/java/me/codedred/playtimes/listeners/Join.java
+++ src/main/java/me/codedred/playtimes/listeners/Join.java
@@ -3,11 +3,11 @@ package me.codedred.playtimes.listeners;
 import me.codedred.playtimes.data.DataManager;
 import me.codedred.playtimes.statistics.StatManager;
 import me.codedred.playtimes.statistics.StatisticType;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import java.util.Objects;
 import java.util.UUID;
 
 public class Join implements Listener {
@@ -15,17 +15,23 @@ public class Join implements Listener {
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
 		DataManager data = DataManager.getInstance();
-		if (data.getData().contains("blocked." + event.getPlayer().getName().toLowerCase()))
-			return;
-
-		UUID uuid = event.getPlayer().getUniqueId();
-		if (data.getData().getConfigurationSection("leaderboard").contains(uuid.toString())) {
+		if (data.getData().contains("blocked." + event.getPlayer().getName().toLowerCase())) {
 			return;
 		}
+
+		UUID uuid = event.getPlayer().getUniqueId();
+		ConfigurationSection leaderboardSection = data.getData().getConfigurationSection("leaderboard");
+
+		if (leaderboardSection == null) {
+			leaderboardSection = data.getData().createSection("leaderboard");
+		}
+
+		if (leaderboardSection.contains(uuid.toString())) {
+			return;
+		}
+
 		long time = StatManager.getInstance().getPlayerStat(uuid, StatisticType.PLAYTIME);
-		data.getData().set("leaderboard." + uuid, time);
+		leaderboardSection.set(uuid.toString(), time);
 		data.saveData();
-
 	}
-
 }
