ff0670c65c6725a27dc2f7f93d0a434f23bd8b72
CodedRed <devcodedred@gmail.com>
2024-06-18 23:11:24 -0500

    add release script

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NEW_VERSION="1.6.2"
+
+git checkout main
+git pull origin main
+
+git tag -a $NEW_VERSION -m "Release version $NEW_VERSION"
+
+git push origin $NEW_VERSION
+
+git checkout production
+git pull origin production
+
+git merge main -m "Release $NEW_VERSION"
+
+git push origin production
+
+echo "Release $NEW_VERSION completed and merged into production."
+read -p "Press any key to continue... "
